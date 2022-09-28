fb6bbec9b22e554689a38470a9de0519421bae21
finnbernhold <finn@gitarren-und-lampen.de>
2022-09-28 17:18:27 +0200

    initial commit

---
+++ src/main/java/com/finnbernhold/leihothek/LeihothekApplication.java
@@ -0,0 +1,14 @@
+package com.finnbernhold.leihothek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeihothekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeihothekApplication.class, args);
+	}
+
+}
+
