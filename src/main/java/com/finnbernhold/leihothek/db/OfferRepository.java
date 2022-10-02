55befab09d9e4289c7327ad5b84009e61a5fa63f
finnbernhold <finn@gitarren-und-lampen.de>
2022-10-02 18:42:48 +0200

    Merge branch 'main' of /home/finn/git/intellij/leihothek with conflicts.

--- src/main/java/com/finnbernhold/leihothek/db/OfferRepository.java
+++ src/main/java/com/finnbernhold/leihothek/db/OfferRepository.java
@@ -3,5 +3,8 @@ package com.finnbernhold.leihothek.db;
 import com.finnbernhold.leihothek.offer.Offer;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OfferRepository extends CrudRepository<Offer, Integer> {
+    List<Offer> findAllByOrderByIdAsc();
 }
