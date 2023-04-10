47da4f1d201805ab04c5d0f01d16c58d16a63d84
Finn <finn@gitarren-und-lampen.de>
2023-04-10 14:44:22 +0200

    fixed image orientation

--- src/main/java/com/finnbernhold/leihothek/offer/image/ImageResize.java
+++ src/main/java/com/finnbernhold/leihothek/offer/image/ImageResize.java
@@ -4,8 +4,6 @@ package com.finnbernhold.leihothek.offer.image;
 import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.stereotype.Component;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -16,13 +14,13 @@ public class ImageResize {
     byte[] resizeImage(byte[] image) throws IOException {
 
         InputStream inputStream = new ByteArrayInputStream(image);
-        BufferedImage originalImage = ImageIO.read(inputStream);
-
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        Thumbnails.of(originalImage)
-                .size(originalImage.getWidth(), originalImage.getHeight())
+        Thumbnails.of(inputStream)
+                .width(1000)
+                .keepAspectRatio(true)
                 .outputFormat("JPEG")
                 .outputQuality(0.5)
+                .useExifOrientation(true)
                 .toOutputStream(outputStream);
 
         return outputStream.toByteArray();
